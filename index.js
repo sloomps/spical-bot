d9f2a0679500b427f79ea1577a002a7248367043
sloom <abanalhwamdt@gmail.com>
2026-07-03 07:52:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { token } = require('./config.json');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.commands = new Collection();
+
+// قراءة الأوامر تلقائياً من المجلدات
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+	}
+}
+
+// تشغيل البوت
+client.login(token);
